9f11cf08d28046d5e6f60658819ba27316d6bc20
Yuktha04 <128046047+Yuktha04@users.noreply.github.com>
2024-07-27 01:38:29 +0530

    stage1

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@
 import React from 'react';
 import './App.css';
 import Dropdown from './Dropdown';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navigation from './Navigation';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
 
 function App() {
     return (
-        <div className="App">
-            <h1>Dropdown Box Example</h1>
-            <Dropdown />
-        </div>
+        <Router>
+            <div className="App">
+                <Navigation />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/contact" element={<Contact />} />
+                </Routes>
+                <h4>Dropdown Box Example</h4>
+                <Dropdown />
+
+            </div>
+        </Router>
     );
 }
 
